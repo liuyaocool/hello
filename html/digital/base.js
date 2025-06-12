9bb202dfa1656266b5dfbb0471e61d797206eaba
liuyao <ly@gm.com>
2025-06-12 12:48:27 +0000

    phone

---
+++ html/digital/base.js
@@ -0,0 +1,15 @@
+async function getPhone() {
+    let phone = {};
+    let phoneList = ['huawei.json', 'honor.json', 'oppo.json', 'vivo.json'];
+    for (let i = 0, tmp; i < phoneList.length; i++) {
+        tmp = await this.getJsonFile('phone/' + phoneList[i]);
+        for(let k in tmp) {
+            phone[k] = tmp[k];
+        }
+    }
+    return phone;
+}
+
+async function getJsonFile(path) {
+    return JSON.parse(await fetchTextFile(path));
+}
